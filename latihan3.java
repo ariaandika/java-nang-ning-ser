9e4fae7271ec2cdfa7af650857dcb2213f8c13ee
ariaandika <andikaakun17@gmail.com>
2024-03-18 20:03:35 +0700

    add lat3

---
+++ latihan3.java
@@ -0,0 +1,9 @@
+class Latihan3 {
+    public static void main(String[] args) {
+        Mahasiswa mhs1 = new Mahasiswa();
+        mhs1.nim = "A11.222.14484";
+        mhs1.nama = "Aria";
+        mhs1.Tampilkan(mhs1.nim, mhs1.nama);
+    }
+}
+
